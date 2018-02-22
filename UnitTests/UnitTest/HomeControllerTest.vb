786dd2e90594ab76075ee6e5bc8d7f01ce88ad72
Leonardo Alves <leoallvez@hotmail.com>
2018-02-22 14:49:12 -0300

    first commit

---
+++ UnitTests/UnitTest/HomeControllerTest.vb
@@ -0,0 +1,8 @@
+﻿Public Class HomeControllerTest
+    [TestMethod]
+        Public void SampleTest()
+        {
+            Assert.AreEqual("HomeController", "HomeController");
+        }
+
+End Class
---
+++ UnitTests/UnitTestsLab/HomeControllerTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestsLab
+{
+    class HomeControllerTest
+    {
+        [TestMethod]
+        public void SampleTest()
+        {
+            Assert.AreEqual("HomeController", "HomeController");
+        }
+    }
+}
